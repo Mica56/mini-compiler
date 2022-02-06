f6eff07edf43fa904e7c93dd3d01a508350feaf9
cuevasdevinkyle <98320743+cuevasdevinkyle@users.noreply.github.com>
2022-02-06 13:39:11 +0800

    updatekeyword

--- lexanal.c
+++ lexanal.c
@@ -46,7 +46,6 @@ bool validIdentifier(char* str)
     return (true);
 }
  
-//edit this 
 //We don't have the ff. keywords: continue, sizeof, short, typedef, void, static, and struct
 // Returns 'true' if the string is a KEYWORD.
 bool isKeyword(char* str)
