0c507c0e0938dbc163958aee4869d27199df0c22
cuevasdevinkyle <98320743+cuevasdevinkyle@users.noreply.github.com>
2022-02-09 13:00:38 +0800

    TOExpression
    Tried making it but for some reason it's bugged at printing where it also prints equals as operator and exclamation marks as identifiers

--- lexanal.c
+++ lexanal.c
@@ -3,48 +3,97 @@
 #include <string.h>
 #include <stdlib.h>
 
-<<<<<<< Updated upstream
-//check if this is okay @Jessie
+
+int CURRENT_LINE = 1 ; 
+
 // Returns 'true' if the character is a DELIMITER.
-bool isDelimiter(char ch)
-=======
-//Returns a string stating what the delimiter is
 const char* isDelimiter(char ch)
->>>>>>> Stashed changes
 {
-	char intro[] = "DELIMITER IS";
-	
-	switch(ch){
-		case ',': return strncat(intro,"COMMA",5);
-		case ';': return strncat(intro,"SEMICOLON",9);
-		case '(': return strncat(intro,"PARENTHESIS_LEFT",16);
-		case ')': return strncat(intro,"PARENTHESIS_RIGHT",17);
-		case '[': return strncat(intro,"BRACKET_LEFT",12);
-		case ']': return strncat(intro,"BRACKET_RIGHT",13);
-		case '{': return strncat(intro,"CURLY_BRACKET_LEFT",18);
-		case '}': return strncat(intro,"CURLY_BRACKET_RIGHT",19);
-		default: return NULL;
-	}
+    if (ch == ' ') 
+		return "Delimiter_Blankspace";
+	else if (ch == ';') 
+		return "Delimiter_Semicolon";
+	else if (ch == '(')
+		return "Delimiter_ParenLeft";
+	else if (ch == ')')
+		return "Delimiter_ParenRight";
+    else if (ch == '[')
+		return "Delimiter_BracketLeft";
+	else if (ch == ']')
+		return "Delimiter_BracketRight";
+	else if (ch == '{')  
+		return "Delimiter_BracesLeft";
+	else if (ch == '}')
+		return "Delimiter_BracesRight";
+	else if (ch == '+')
+		return "Delimiter";
+	else if (ch == '-')
+		return "Delimiter";
+	else if (ch == '*')
+		return "Delimiter";
+	else if (ch == '/')
+		return "Delimiter";
+	else if (ch == ',')
+		return "Delimiter";
+	else if (ch == '>')
+		return "Delimiter";
+	else if (ch == '<')
+		return "Delimiter";
+	else if (ch == '=')
+		return "Delimiter";
+  return "Not a Delimiter";
 }
+
+//Returns string if two chars are TO Expressions
+ const char* isTOExpression(char left, char right){
+ 	char a = left;
+	char b = right;
+	
+	//Evaluates if a and b are TO expressions by comparing the two simultaneously
+	 if(a == '=' && b == '=')
+ 		return "Equal to Operator";
+ 	else if(a == '!' && b == '=')
+	 	return "Not equal to operator";
+	else if(a == '<' && b == '=')
+		return "Less than or equal to operator";
+	else if(a == '>' && b == '=')
+		return "Greater than or equal to operator";
+	return "Not a TO Expression";	
+} 
 
 //edit this
 // Returns 'true' if the character is an OPERATOR.
-bool isOperator(char ch)
+const char* isOperator(char ch)
 {
-    if (ch == '+' || ch == '-' || ch == '*' ||
-        ch == '/' || ch == '>' || ch == '<' ||
-        ch == '=')
-        return (true);
-    return (false);
+    if (ch == '+')
+		return "Operator_plus";
+	else if (ch == '-')
+		return "Operator_minus";
+	else if (ch == '*')
+		return "Operator_multiply";
+	else if (ch == '/')
+        return "Operator_divide";
+	else if (ch == '>')
+		return "Operator_greaterthan";
+	else if (ch == '<') 
+		return "Operator_lessthan";
+	else if (ch == '=')
+        return "Operator_equals";
+    return "Not an Operator";
 }
  
 // Returns 'true' if the character is an CHEMICAL OPERATOR.
-bool isChemOperator(char ch)
-{
-    if (ch == '~' || ch == '^' || ch == '[' ||
-        ch == ']')
-        return (true);
-    return (false);
+const char* isChemOperator(char ch){
+	
+    if (ch == '~')
+    	return "ChemOperator_tilde";	
+	else if (ch == '^')
+		return "ChemOperator_carat";
+	else if (ch == '[') 
+		return "ChemOperator_BracketLeft";
+	else if (ch ==']')
+		return "ChemOperaror_BracketRight";
+    return "Not a ChemOperator";
 }
 
 // Returns 'true' if the string is a VALID IDENTIFIER.
@@ -53,7 +102,7 @@ bool validIdentifier(char* str)
     if (str[0] == '0' || str[0] == '1' || str[0] == '2' ||
         str[0] == '3' || str[0] == '4' || str[0] == '5' ||
         str[0] == '6' || str[0] == '7' || str[0] == '8' ||
-        str[0] == '9' || isDelimiter(str[0]) == true) 
+        str[0] == '9' || isDelimiter(str[0]) != "Not a Delimiter")
         return (false);
     return (true);
 }
@@ -140,57 +189,112 @@ void parse(char* str)
 {
     int left = 0, right = 0;
     int len = strlen(str);
+    //const char* value;
+    //
+    FILE *dest_fp;
+    dest_fp = fopen("results.mul","w");
  
     while (right <= len && left <= right) {
-        if (isDelimiter(str[right]) == false)
+                
+        if (isDelimiter(str[right]) == "Not a Delimiter")
             right++;
- 
-        if (isDelimiter(str[right]) == true && left == right) {
-            if (isOperator(str[right]) == true)
-                printf("'%c' IS AN OPERATOR\n", str[right]);
-            else if(isChemOperator(str[right]) == true)
-            	printf("'%c' IS A CHEMICAL OPERATOR\n", str[right]);
+            
+        //Takes two consecutive characters and send them to isTOExpression 
+		if (isTOExpression(str[left],str[right]) != "Not a TO Expression")
+            printf("TO EXPRESSION '%s'\n", isTOExpression(str[right-1],str[right]));
+		        
+        if (isDelimiter(str[right]) != "Not a Delimiter" && left == right) {
+        	printf("%s\n", isDelimiter(str[right]));
+            fprintf(dest_fp,"%d %d %c %s\n",CURRENT_LINE,right,str[right], isDelimiter(str[right]));
+            
+            
+          
+            if (isOperator(str[right]) != "Not an Operator") {
+                printf("%s\n", isOperator(str[right]));
+                fprintf(dest_fp,"%d %d %c %s\n",CURRENT_LINE,right,str[right], isOperator(str[right]));
+            }
+            else if(isChemOperator(str[right]) != "Not a ChemOperator") {
+            	printf("%s\n", isChemOperator(str[right]));
+                fprintf(dest_fp,"%d %d %c %s\n",CURRENT_LINE,right,str[right], isChemOperator(str[right]));
+            }
  
             right++;
             left = right;
-        } else if (isDelimiter(str[right]) == true && left != right
+        } else if (isDelimiter(str[right]) != "Not a Delimiter" && left != right
                    || (right == len && left != right)) {
             char* subStr = subString(str, left, right - 1);
  
-            if (isKeyword(subStr) == true)
-                printf("'%s' IS A KEYWORD\n", subStr);
+            if (isKeyword(subStr) == true) { 
+                printf("KEYWORD '%s'\n", subStr);
+                fprintf(dest_fp,"%d %d %s %s\n",CURRENT_LINE,right, subStr, "KEYWORD");
+            }
  
             else if (isInteger(subStr) == true)
-                printf("'%s' IS AN INTEGER\n", subStr);
+                printf("INTEGER '%s'\n", subStr);
  
             else if (isRealNumber(subStr) == true)
-                printf("'%s' IS A REAL NUMBER\n", subStr);
-            
+                printf("REAL NUMBER '%s'\n", subStr);
+                
+                
             else if (validIdentifier(subStr) == true
-                     && isDelimiter(str[right - 1]) == false
-					 && isChemOperator(str[right - 1]) == false)
-                printf("'%s' IS A VALID IDENTIFIER\n", subStr);
+                     && isDelimiter(str[right - 1]) == "Not a Delimiter"
+					 && isChemOperator(str[right - 1]) == "Not a ChemOperator") {
+                printf("IDENTIFIER '%s'\n", subStr);
+                fprintf(dest_fp,"%d %d %s %s\n",CURRENT_LINE,right, subStr, "IDENTIFIER");
+            }
             
            else if (validIdentifier(subStr) == true
-					 && isChemOperator(str[right - 1]) == true)
-                printf("'%s' IS A CHEMICAL OPERATOR\n", subStr);
+					 && isChemOperator(str[right - 1]) != "Not a ChemOperator") {
+                printf("%s\n", isChemOperator(str[right - 1]));
+                fprintf(dest_fp,"%d %d %s\n",CURRENT_LINE,right, isChemOperator(str[right-1]));
+           }
 
             else if (validIdentifier(subStr) == false
-                	&& isDelimiter(str[right - 1]) == false)
+                	&& isDelimiter(str[right - 1]) == "Not a Delimiter")
                 printf("'%s' IS NOT A VALID IDENTIFIER\n", subStr);
             left = right;
         }
     }
+
+    CURRENT_LINE++;
     return;
 }
- 
+
 // DRIVER FUNCTION
 int main()
 {
      // maximum length of string is 100 here
-    char str[100] = "int .a_nt += 1 + m1 + ~[H^2O]";//has a bug with H^20
- 
-    parse(str); // calling the parse function
- 
+     // Global Variables 
+    bool DEBUG = true ;
+
+    // DEBUG Mode: Just for minor line tests
+    if (DEBUG) {
+        char str[100] = ">=";//has a bug with other delimiters and identifiers where they also appear along with the display of TO
+
+        parse(str); // calling the parse function
+    }
+    // DEBUG-OFF Mode: Accepts an entire file and converts it all into another file containing lexeme tokens.
+    else {
+        char line[100] ;
+        size_t len = 0 ;
+        FILE *fptr;
+        ssize_t read;
+        char *val;
+
+
+        fptr = fopen("file.mul","r");
+
+        if (fptr == NULL) {
+            printf("Can't Open File");
+            return 0 ;
+        }
+
+        while (val = fgets(line,100, fptr)) {
+            parse(val);
+        }
+
+        fclose(fptr);
+    }
+
     return (0);
 }
